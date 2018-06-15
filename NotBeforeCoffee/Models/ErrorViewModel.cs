42a15b9f5e8ca5001c8e5976087015cdf777456a
FallenDev <paul_buck86@hotmail.com>
2018-06-15 08:08:57 -0400

    Initial

---
+++ NotBeforeCoffee/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NotBeforeCoffee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
